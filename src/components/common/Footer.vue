19d7222e3687953287df69f99470c8e5802077b4
mj1331313 <author@2238178090@qq.com>
2017-11-23 19:25:40 +0800

    update

---
+++ src/components/common/Footer.vue
@@ -0,0 +1,29 @@
+<template>
+    <ul class="footer">
+      <li>电影</li>
+      <li>音乐</li>
+      <li>书籍</li>
+      <li>图片</li>
+    </ul>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style scoped>
+.footer{
+  position: fixed;
+  bottom: 0;
+  background-color: rgb(33, 150, 243);
+  color: #fff;
+  display: flex;
+  width: 100%;
+}
+.footer li{
+  flex-grow: 1;
+  height: 1rem;
+  line-height: 1rem;
+  text-align: center;
+}
+</style>
---
+++ src/components/movie/Movie.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="movie">
+        <common-header></common-header>
+        <router-view></router-view> 
+        <common-footer></common-footer>       
+    </div>
+</template>
+<script>
+import CommonHeader from '../common/Header'
+import CommonFooter from '../common/Footer'
+export default {
+  components: {
+      CommonHeader,
+      CommonFooter
+  }
+}
+</script>
+<style scoped>
+
+</style>
