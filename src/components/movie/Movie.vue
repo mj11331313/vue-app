d88ef540adc1991f226e1ad00e029e4a0e7bf25e
skygodbas <2238178090@qq.com>
2017-11-28 13:46:00 +0800

    Update Movie.vue

--- src/components/movie/Movie.vue
+++ src/components/movie/Movie.vue
@@ -1,8 +1,11 @@
 <template>
     <div id="movie">
-        <common-header></common-header>
+        <common-header bgColor="saddlebrown">
+            <!--将原头部组件的slot位置用span待替，使用slot属性对应slot标签name值完成替换:-->
+            <span slot="header">Movie</span>
+        </common-header>
         <router-view></router-view> 
-        <common-footer></common-footer>       
+        <common-footer  bgColor="saddlebrown"></common-footer>       
     </div>
 </template>
 <script>
