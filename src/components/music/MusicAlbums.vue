a04c6a7507a5737a4f696f11d072d0fe01d4ab93
skygodbas <2238178090@qq.com>
2017-11-28 13:44:36 +0800

    Create MusicAlbums.vue

---
+++ src/components/music/MusicAlbums.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="albums">
+        <!--这里必须使用v-if,让播放组件在数据准备好之前先不显示，否则会报错！-->
+        <a-player v-if="isShow" :showlrc="3" :autoplay="true" :music="songs"></a-player>       
+    </div>
+</template>
+<script>
+// 引入vue-aplayer插件：
+import VueAplayer from "vue-aplayer"
+import Axios from "axios"
+export default {
+    data(){
+        return {
+            songs: [],
+            isShow: false,
+        }
+    },
+    components: {
+        // 使用a-play播放器组件：
+        'a-player': VueAplayer  // 使用vue-aplayer的标签名为a-player
+    },
+    mounted(){
+        Axios.get('/static/data/musicdata.json').then(res => {
+            let list = res.data.musicData;
+            list.forEach(element => {
+                this.songs.push({
+                    title: element.title,
+                    author: element.author,
+                    url : element.src,
+                    pic : element.musicImgSrc,
+                    lrc : '/static/'+element.lrc
+                });
+            });
+            this.isShow = true;
+        })
+    }
+}
+</script>
+<style scoped>
+.albums{
+    margin: 1rem .2rem;
+}
+</style>
