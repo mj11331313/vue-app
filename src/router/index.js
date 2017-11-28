32297c749c7ab30c887be53cff40209db8e6393b
skygodbas <2238178090@qq.com>
2017-11-28 13:42:46 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,11 @@ import Router from 'vue-router'
 import Movie from '@/components/movie/Movie'
 import MovieList from '@/components/movie/MovieList'
 import MovieDetail from '@/components/movie/MovieDetail'
+import Music from '@/components/music/Music'
+import MusicList from '@/components/music/MusicList'
+import MusicAlbums from '@/components/music/MusicAlbums'
+import Book from '@/components/book/Book'
+import Photo from '@/components/photo/Photo'
 Vue.use(Router)
 
 export default new Router({
@@ -11,16 +16,41 @@ export default new Router({
     {
       path: '/',
       name: 'Movie',
+      redirect: '/movielist',
       component: Movie,
       children: [
         {
           path: 'movielist',
+          name:'movielist',
           component: MovieList
         }, {
           path: 'moviedetail/:movieId',  // 通过url传参
           component: MovieDetail
         }
       ]
+    }, {
+      path: '/music',
+      name: 'music',
+      component: Music,
+      redirect: '/music/musiclist',
+      children: [
+        {
+          path: 'musiclist',
+          component: MusicList
+        }, {
+          path: 'musicalbums/:musicId',
+          component: MusicAlbums
+        }
+      ]
+    }, {
+      path: '/book',
+      name: 'book',
+      component: Book
+    }, {
+      path: '/photo',
+      name: 'photo',
+      component: Photo
     }
   ]
 })
+
