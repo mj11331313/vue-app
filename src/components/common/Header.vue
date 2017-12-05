8a80c7f17c544092abd9b7a7a4fb34c459534a63
skygodbas <cynthia_hfe@163.com>
2017-12-05 19:43:48 +0800

    Update Header.vue

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -1,12 +1,23 @@
 <template>
-  <div class="header">
-    <button class="home">首页</button>
-    <h1>Movie</h1>
+  <!--动态获取style值，其值必须为一个对象：-->
+  <div class="header" :style="{background: bgColor}"> <!--bgColor为自定义1背景色变量-->
+    <span class="home" @click="goHome">首页</span>
+    <h1>
+      <!--使用slot（插槽）标签,方便将来完成标题参数传递：-->
+      <slot name="header">默认值</slot>
+    </h1>
   </div>
 </template>
 <script>
 export default {
-  
+  methods: {
+    goHome(){
+      this.$router.push('/movielist');
+    }
+  },
+  // 组件间传值必须配置props,且其值为数组：
+  props: ['bgColor']
+  // 改变某个属性值使用props，改变内容使用slot
 }
 </script>
 <style scoped>
@@ -19,8 +30,8 @@ export default {
   line-height: 1rem;  
 }
 .home{
-  background-color: rgb(33, 150, 243);
-  border: none;
+  background-color: transparent;
+  margin-left: .2rem;
 }
 .home,.header h1{
   color: #fff;
