5f30c343aa11997c45c7d140e3f52abee6492749
skygodbas <cynthia_hfe@163.com>
2017-12-05 19:43:22 +0800

    Update Footer.vue

--- src/components/common/Footer.vue
+++ src/components/common/Footer.vue
@@ -1,22 +1,20 @@
 <template>
-    <ul class="footer">
-      <li>电影</li>
-      <li>音乐</li>
-      <li>书籍</li>
-      <li>图片</li>
+    <ul class="footer" :style="{background: bgColor}">
+      <li><router-link to="/movielist">电影</router-link></li>
+      <li><router-link to="/music">音乐</router-link></li>
+      <li><router-link to="/book">书籍</router-link></li>
+      <li><router-link to="/photo/photolist">图片</router-link></li>
     </ul>
 </template>
 <script>
 export default {
-  
+  props: ['bgColor']
 }
 </script>
 <style scoped>
 .footer{
   position: fixed;
   bottom: 0;
-  background-color: rgb(33, 150, 243);
-  color: #fff;
   display: flex;
   width: 100%;
 }
@@ -25,5 +23,13 @@ export default {
   height: 1rem;
   line-height: 1rem;
   text-align: center;
+}
+.footer li a{
+  color: darkgray;
+  outline: none;
+}
+/*vue会自动给当前点击的链接添加class="router-link-active"：*/
+.footer li a.router-link-active{
+  color: #fff;
 }
 </style>
