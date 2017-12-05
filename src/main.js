e05ae747ddb321bb1ea66ef64747520603dfb7ae
skygodbas <cynthia_hfe@163.com>
2017-12-05 19:50:26 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,13 +5,21 @@ import App from './App'
 import router from './router'
 import './assets/css/reset.css'
 import './assets/js/rem.js'
-Vue.config.productionTip = false
+import store from '@/vuex/store'
+// vue-touch：vuejs下的移动端事件判断插件
+import VueTouch from 'vue-touch'
+
+// 只有将vue-touch重命名为v-touch后才可利用v-touch标签来使用vue-touch插件：
+Vue.use( VueTouch,{ name:'v-touch'})
+
+Vue.config.productionTip = false;
 // 代理地址：
 global.API_PROXY = 'https://bird.ioliu.cn/v1/?url='
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,  // 配置store,此处不配置将不能使用$store
   template: '<App/>',
   components: { App }
 })
