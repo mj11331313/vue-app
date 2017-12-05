151ac2152ddbdfdefe0f271678f72870a4bb12b7
mj1331313 <author@2238178090@qq.com>
2017-12-05 20:03:00 +0800

    update

---
+++ src/vuex/store.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+Vue.use(Vuex)
+
+const state = {
+    photoList: []
+};
+
+const mutations = {
+    setPhotoList(state,status){
+        state.photoList = status;
+    }
+};
+
+const actions = {
+    setPhotoList({commit},status) {
+        commit('setPhotoList',status);
+    }
+};
+
+
+export default new Vuex.Store({
+    state,
+    mutations,
+    actions
+});
