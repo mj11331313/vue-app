a15ee915605510130e21914f46dbedbbbb00d03d
skygodbas <cynthia_hfe@163.com>
2018-03-20 21:19:49 +0800

    Update MusicAlbums.vue

--- src/components/music/MusicAlbums.vue
+++ src/components/music/MusicAlbums.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="albums">
         <!--这里必须使用v-if,让播放组件在数据准备好之前先不显示，否则会报错！-->
-        <a-player v-if="isShow" :showlrc="3" :autoplay="true" :music="songs"></a-player>       
+        <a-player v-if="isShow" autoplay mutex ref="player" :music="songs"></a-player>       
     </div>
 </template>
 <script>
@@ -20,23 +20,21 @@ export default {
         'a-player': VueAplayer  // 使用vue-aplayer的标签名为a-player
     },
     beforeRouteLeave: ((to, from, next) => {
-        // location.href = to.fullPath;
         next();        
     }),
     mounted(){
-        Axios.get('/static/data/musicdata.json').then(res => {
-            let list = res.data.musicData;
-            list.forEach(element => {
+        Axios.get( API_PROXY2 + 'song?id='+this.$route.params.music).then(res => {
+                // console.log(res.data.data);
                 this.songs.push({
-                    title: element.title,
-                    author: element.author,
-                    url : element.src,
-                    pic : element.musicImgSrc,
-                    lrc : '/static/'+element.lrc
+                    title: res.data.data.name,
+                    author: res.data.data.ar[0].name,
+                    url : res.data.data.mp3.url,
+                    pic : res.data.data.al.picUrl,
                 });
-            });
-            this.isShow = true;
-        })
+                this.isShow = true;     
+            }).catch(() => {
+                console.log('请求失败');
+        });
     }
 }
 </script>
